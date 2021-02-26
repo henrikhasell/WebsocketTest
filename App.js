ae39a8be589efb47a85b602082ccc02b11e8cec0
Henrik Hasell <henrikhasell@gmail.com>
2021-02-26 18:26:25 +0000

    update project

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import io from 'socket.io-client';
 
-const socket = io('http://desktop.croydon.vpn:9090', {pingTimeout: 60000});
+const socket = io('http://51.19.126.93:9090', {
+  autoConnect: false
+});
+
+// Debug code, 
+const log = console.warn;
+socket.on('connect', () => log('Socket is connected'));
+socket.on('disconnect', () => log('Socket is disconnected'));
 
 export default function App() {
   const [currentMessage, setCurrentMessage] = useState('');
   const [chatMessages, setChatMessages] = useState([]);
+  const [connected, setConnected] = useState(false);
+
+  const addMessage = message => setChatMessages([...chatMessages, message]);
+
+  const sendMessage = message => socket.emit('chat-message', message);
 
   const submitChatMessage = () => {
-    socket.emit('chat-message', currentMessage);
+    sendMessage(currentMessage);
     setCurrentMessage('');
   };
 
+  useEffect(() => {
+    socket.on('connect', () => setConnected(true));
+    socket.on('disconnect', () => setConnected(false));
+    socket.on('chat-message', addMessage);
+    socket.open();
+  }, [chatMessages]);
+
   return (
     <View style={styles.container}>
-      {chatMessages.map(message => <Text>{message}</Text>)}
+      <Text>Messages Appear Below</Text>
+      <Text>{connected ? 'Connected :)' : 'Diconnected :('}</Text>
+      {chatMessages.map((message, index) => <Text key={index}>{message}</Text>)}
       <TextInput
+        style={styles.text_input}
         value={currentMessage}
         onSubmitEditing={submitChatMessage}
         onChangeText={setCurrentMessage}/>
@@ -32,4 +54,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text_input: {
+    borderWidth: 1,
+    width: 100
+  }
 });
